b787273affbc14b4efa1bc1353b723fc07b783ce
Salah AlDin <salahqerem@gmail.com>
2024-03-31 17:44:06 +0200

    Add project models

---
+++ DB/models/user.model.js
@@ -0,0 +1,35 @@
+import { Schema, model } from "mongoose";
+
+const UserSchema = new Schema(
+  {
+    userName: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+
+    confirmEmail: {
+      type: Boolean,
+      default: false,
+    },
+    age: Number,
+    gender: {
+      type: String,
+      enum: ["Male", "Female"],
+      default: "Male",
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const userModel = model("User", UserSchema);
+export default userModel;
